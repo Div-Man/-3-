23893e586f2685badb8ef1589360dc85bf23a724
Dimon2x <dkakharov@yandex.ru>
2016-07-08 12:57:40 +0400

    first commit

---
+++ function.js
@@ -0,0 +1,33 @@
+
+function consoleRec(array, task){
+	console.log(array[0] + task);
+
+
+
+	if (array[0]){
+		console.log(array[1] + task);
+			if (array[1]){
+			console.log(array[2] + task);
+				if (array[2]){
+					console.log(array[3] + task);
+						if (array[3]){
+							console.log(array[4] + task);
+				}
+			}
+			
+		}
+	}
+}
+
+
+module.exports = consoleRec
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,3 @@
+var consoleRec = require('./function.js')
+
+consoleRec(['я', 'умею', 'писать', 'рекурсивные', 'функции'], " - выполнено")
